685bec2ad4e621b1da46efb9692f3f88211d00c4
Mohamed Dief <mdaif1332@gmail.com>
2021-02-10 01:52:04 +0200

    Remove Development Comments From Handler.
    During Developing The Error Handler I Forgot To Remove The Test Comment From The File.

--- assets/ErrorHandler.py
+++ assets/ErrorHandler.py
@@ -15,21 +15,3 @@ class classErrorHandler:
         Message += f"\n\t{Pointer}{str('^')}"
         Message += f"\n\t{textMessage}"
         print(colored(Message , 'red'))
-
-
-'''
-if __name__ == '__main__':
-    init()
-
-    Call = globals()['ErrorHandler']()
-    Function = getattr(Call, 'CallErrorMessage')
-
-    Function(
-        Code="TEST file.com",
-        charLength=0,
-        lineNumber=4,
-        Path="D:/Projects/EasyShell/dev.easy",
-        Name="UndefinedVariable",
-        textMessage="The Variable You Used Doesn't Exists"
-    )
-'''
